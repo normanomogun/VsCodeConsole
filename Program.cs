309715e5d5072fc5b873fc53988827b00824a7c6
norge <norgee231@yahoo.com>
2021-04-09 11:37:34 +0100

    Added new console writeline

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace VsCodeConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
         }
     }
 }
